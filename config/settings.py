e4f1ccf35c6029d5dfe2bd7c1115672604696a69
m0n0l1t <mon0l1t@bk.ru>
2020-06-28 19:45:27 +0500

    рефакторинг и возможность начать заново

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,3 @@
 settings = {
-    "token": "your token"
+    "token": "token"
 }
--- main.py
+++ main.py
@@ -1,49 +1,75 @@
 import discord
+import random
 from discord.ext import commands
 from config.settings import *
 from models import *
-from sqlalchemy import func
+import os
 
 bot = commands.Bot(command_prefix='')
+black_list = [0, 1, 2]
+operation = 0
 
 
 def last_letter(town):
     n = len(town)-1
-    while town[n] == 'ы' or town[n] == 'ь' or town[n] == 'ъ':
+    while town[n] == 'ы' or town[n] == 'ь' or town[n] == 'ъ' or town[n] == ')':
         n -= 1
     return town[n]
 
 
+def search_last():
+    last = session.query(City).filter_by(is_last=1).first()
+
+    if last is not None:
+        return last
+    else:
+        city_list = session.query(City).all()
+        last = random.choice(city_list)
+        last.is_used = 1
+        last.is_last = 1
+        session.commit()
+        return last
+
+
+def clear_base():
+    used = session.query(City).filter_by(is_used=1).all()
+    for i in used:
+        i.is_used = 0
+        i.is_last = 0
+        session.commit()
+
+
 @bot.event
 async def on_message(message):
-    message.content = message.content.lower()
+    if bot.user.id == message.author.id:
+        return
+    msg = message.content.lower()
+    if msg == 'сначала':
+        clear_base()
+        last_city = search_last()
+        await message.channel.send("Начнем с города **[" + last_city.name.upper() + "]**")
+        await message.channel.send("Следующий город на букву [" + last_letter(last_city.name) + "]")
 
-    try:
-        last = session.query(City).filter_by(is_last=1).first()
+    if message.author.id not in black_list:
 
-        try:
-            ourCity = session.query(City).filter_by(name=message.content).first()
-            if bot.user.id != message.author.id and ourCity is None:
-                await message.channel.send("Такого города не существует")
-            else:
-                if last_letter(last.name) == ourCity.name[0]:
-                    if ourCity.is_used == 0:
-                        ourCity.is_used = 1
-                        ourCity.is_last = 1
-                        last.is_last = 0
-                        session.commit()
-                        channel = message.channel
-                        await channel.send("Следующий город на букву ["+last_letter(ourCity.name)+"]")
-                    else:
-                        channel = message.channel
-                        await channel.send("Этот город уже был")
+    else:
+
+        our_city = session.query(City).filter_by(name=msg).first()
+        last_city = search_last()
+        if our_city is None:
+            await message.channel.send("Такого города не существует")
+
+        else:
+            if last_letter(last_city.name) == our_city.name[0]:
+                if our_city.is_used == 0:
+                    our_city.is_used = 1
+                    our_city.is_last = 1
+                    last_city.is_last = 0
+                    await message.channel.send("Следующий город на букву [" + last_letter(our_city.name) + "]")
+                    session.commit()
                 else:
-                    channel = message.channel
-                    await channel.send("Этот город не начинается на букву ["+last.name[len(last.name)-1]+"]")
-        except ValueError:
-            print("ошибка")
-    except ValueError:
-        print("ошибка")
-
+                    await message.channel.send("Этот город уже был")
+            else:
+                await message.channel.send("Этот город не начинается на букву [" + last_letter(last_city.name) + "]")
 
 bot.run(settings["token"])
